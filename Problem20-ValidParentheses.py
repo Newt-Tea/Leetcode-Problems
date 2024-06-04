7bf3360650fe54c8a756b4e709bff6f8bb8e1119
hhargrove <hudsonhargrove01@gmail.com>
2024-06-04 13:54:03 -0500

    saving for later conditional solution will not work for this problem

--- Problem20-ValidParentheses.py
+++ Problem20-ValidParentheses.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
+        parOpen = False 
+        curlOpen = False
+        bracOpen = False
+
         for char in s:
             if (char=='('):
                 parOpen = True
@@ -11,22 +16,27 @@ class Solution(object):
                 curlOpen = True
             elif(char == '['):
                 bracOpen = True
-            elif(char == ')' and parOpen):
+            elif(char == ')' and parOpen and prev == '('):
                 parOpen = False
-            elif(char == '}' and curlOpen):
+            elif(char == '}' and curlOpen and prev == '{'):
                 curlOpen = False
-            elif(char ==']' and bracOpen):
+            elif(char ==']' and bracOpen and prev == '['):
                 bracOpen = False
+            prev = char
             
-            if(parOpen or curlOpen or bracOpen):
-                return False
-            else: return True
+        if(parOpen or curlOpen or bracOpen):
+            return False
+        else: return True
 
 if __name__ == "__main__":
     answer = Solution()
     test1 = answer.isValid("()")
     test2 = answer.isValid("()[]{}")
     test3 = answer.isValid("(]")
+    test4 = answer.isValid("([)")
+    test5 = answer.isValid("{[]}")
     print(test1)
     print(test2)
     print(test3)
+    print(test4)
+    print(test5)
